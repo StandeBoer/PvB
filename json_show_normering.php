f38a467bae5dfc5f681536fc70d5cbe21c2aff25
JVerheul <jorritv12345@gmail.com>
2017-12-18 09:09:51 +0100

    normering fix

--- json_show_normering.php
+++ json_show_normering.php
@@ -2,15 +2,14 @@
 include 'connect.php';
 $get_normering_id = $_GET['id'];
 //echo $get_cohort_id;
-$get_normering = "SELECT criterium_normering_id, criterium_normering_naam, werkproces_criterium_id FROM criterium_normering WHERE werkproces_criterium_id = '" . $get_normering_id . "'";
+$get_normering = "SELECT criterium_normering_id, criterium_normering_naam FROM criterium_normering WHERE werkproces_criterium_id = '" . $get_normering_id . "'";
 //echo $get_klas;
 $result_criterium_normering = $conn->query($get_normering);
 if ($result_criterium_normering->num_rows > 0) {
     $i = 0;
     while ($row_criterium_normering = $result_criterium_normering->fetch_assoc()) {
-        $return_criterium_normering[$i]["name"] = $row_criterium_normering["criterium_normering_naam"];
+        $return_criterium_normering[$i]["criterium_normering_name"] = $row_criterium_normering["criterium_normering_naam"];
         $return_criterium_normering[$i]["criterium_normering_id"] = $row_criterium_normering["criterium_normering_id"];
-        $return_criterium_normering[$i]["werkproces_criterium_id"] = $row_criterium_normering["werkproces_criterium_id"];
         $i++;
     }
 }
