2973d1372e89b79e226d0c977781e0aebe39693a
Jelmer <jelmerijff@ziggo.nl>
2017-12-20 09:15:21 +0100

    Navbar gefixt

--- navbar.php
+++ navbar.php
@@ -26,6 +26,7 @@
             <li><a class="tekstkleur" href="kerntaken.php">Kerntaken</a></li>
             <li><a class="tekstkleur" href="werkprocessen.php">Werkprocessen</a></li>
             <li><a class="tekstkleur" href="criterium.php">Criterium</a></li>
+            <li><a class="tekstkleur" href="normering.php">Normering</a></li>
             <li><a class="tekstkleur" href="klassen.php">Klassen</a></li>
             <li><a class="tekstkleur" href="studenten.php">Studenten</a></li>
         </ul>
